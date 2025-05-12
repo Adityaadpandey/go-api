7ce1b933756a472d476573f6e2ffec4accbfaed2
Aditya Dutt Pandey <adityapandeyadp@gmail.com>
2025-05-12 19:28:07 +0530

    initial commit

---
+++ cmd/students-api/main.go
@@ -0,0 +1,54 @@
+package main
+
+import (
+	"context"
+	"log"
+	"log/slog"
+	"net/http"
+	"os"
+	"os/signal"
+	"syscall"
+	"time"
+
+	"github.com/adityaadpandey/students-api/internal/config"
+)
+
+func main() {
+	cfg := config.MustLoad()
+
+	router := http.NewServeMux()
+	router.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		if r.Method != http.MethodGet {
+			http.Error(w, "Method Not Allowed", http.StatusMethodNotAllowed)
+			return
+		}
+		w.Write([]byte("Hello, World!"))
+	})
+
+	server := http.Server{
+		Addr:    cfg.Addr,
+		Handler: router,
+	}
+	slog.Info("server startded", "addr", cfg.Addr)
+
+	done := make(chan os.Signal, 1)
+	signal.Notify(done, os.Interrupt, syscall.SIGTERM, syscall.SIGINT)
+
+	go func() {
+		err := server.ListenAndServe()
+		if err != nil {
+			log.Fatalf("failed to start server: %v", err)
+		}
+	}()
+
+	<-done
+
+	slog.Info("shutting down server...")
+	cts, cancel := context.WithTimeout(context.Background(), 5-time.Second)
+	defer cancel()
+	if err := server.Shutdown(cts); err != nil {
+		slog.Error("failed to shutdown server", "error", err)
+	} else {
+		slog.Info("server shut down gracefully")
+	}
+}
